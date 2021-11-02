cb4c2d72171900531c1e33543b5e6275e0e99f6b
Thiago Silva <thiagos.dasilva@gmail.com>
2021-11-02 14:47:04 -0300

    Stop scanning tweets from an invalid context

--- content.js
+++ content.js
@@ -11,34 +11,31 @@ function checkTweets() {
     'div[aria-label="Timeline: Search timeline"] article:not([hidden])')
   for (let i = 0; i < tweetsList.length; i++) {
     let tweet = tweetsList[i]
+    console.log(tweet)
     let tweetTextElement = tweet.querySelector('div[lang][dir="auto"]')
     if (tweetTextElement){
       console.log(tweetTextElement)
       let aTags = tweetTextElement.querySelectorAll('a')
       let  cashtagsCount = 0, hashtagsCount = 0
-      let cashtagsHidden = false, hashtagsHidden = false
       for (let i = 0; i < aTags.length; i++) {
         if (/^\$/.test(aTags[i].textContent)) {
           cashtagsCount++
           if (cashtagsCount > cashAllowed) {
             tweet.parentElement.parentElement.style.display = 'none'
+            tweet.setAttribute('hidden', 1)
             tweetsHidden++
-            cashtagsHidden = true
-            console.log("Cashtags: " + cashtagsHidden)
+            console.log("Cashtags fired")
             break
           }
         } else if (/^#/.test(aTags[i].textContent)) {
           hashtagsCount++
           if (hashtagsCount > hashAllowed) {
             tweet.parentElement.parentElement.style.display = 'none'
+            tweet.setAttribute('hidden', 1)
             tweetsHidden++
-            hashtagsHidden = true
-            console.log("Hashtags: " + hashtagsHidden)
+            console.log("Hashtags fired")
             break
           }
-        }
-        if (cashtagsHidden === true && hashtagsHidden === true) {
-          tweetsHidden--
         }
       }
     }
@@ -54,17 +51,22 @@ function searchListener() {
   })
 }
 
+function InvalidatedContext() {
+  if (chrome.runtime.id == undefined) {
+      location.reload()
+      return true
+  }
+}
+
 window.addEventListener('beforeunload', function (event) {
+  if (InvalidatedContext()) return
   chrome.storage.local.set({ 'tweetsHidden': 0 })
 })
 
 let Observer = new MutationObserver( function() {
   let tweetElement = document.querySelector('article')
   if (tweetElement) {
-    if (chrome.runtime.id == undefined) {
-      location.reload()
-      return
-    }
+    if (InvalidatedContext()) return
     checkTweets()
     searchListener()
   }
